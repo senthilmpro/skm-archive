bf15c694eb105f7e40f86f202c673dc11b2287ca
skm <skm.symc@gmail.com>
2018-09-17 09:17:48 -0700

    sample metadata added

---
+++ api/archive-links.js
@@ -0,0 +1,5 @@
+modules.exports = {
+    "metadata" : "https://archive.org/metadata/{{link-id}}",
+    "profile" : "https://archive.org/details/@{{profile-id}}",
+    "details" : "https://archive.org/details/{{link-id}}"
+}
