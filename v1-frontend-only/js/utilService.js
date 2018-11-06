2a057e6c7575df551928ada644c301b6fe02bfd4
skm-symc-2 <skm.symc@gmail.com>
2018-11-05 17:08:03 -0800

    v1 front end only updated with scripts

---
+++ v1-frontend-only/js/utilService.js
@@ -0,0 +1,42 @@
+app.service('utilService', function(){
+
+    var getWpUri = function(site = 'dumppro.wordpress.com'){
+        return `https://public-api.wordpress.com/rest/v1.1/sites/${site}/posts`;
+    };
+
+    var isValidUri = function(myUri){
+        try {
+            new URL(myUri);
+            return true;
+        } catch(ex){
+            return false;
+        }
+    };
+
+    var replaceDetails = function(str = ""){
+        str = str.replace('/details/', '/metadata/').replace('/download/','/metadata/');
+        return str;
+    }
+    
+    var getLinks = function(posts){
+        if(posts){
+            var urlArr = [];
+            posts.forEach(function(val, index){
+                var title = val.title;
+                if(isValidUri(title)){
+                    title = replaceDetails(title);
+                    urlArr.push(title);
+                }
+            });
+            return urlArr;
+        }
+    }
+    
+    return {
+        getWpUri : getWpUri,
+        isValidUri : isValidUri,
+        replaceDetails : replaceDetails,
+        getLinks : getLinks
+    }
+});
+
